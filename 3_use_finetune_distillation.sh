c8e9f7a2e661973afc5b767916f963e650a73d5a
Mr.P L <pingloaf@gmail.com>
2022-04-18 17:51:07 +0700

    Update 3_use_finetune_distillation.sh

--- 3_use_finetune_distillation.sh
+++ 3_use_finetune_distillation.sh
@@ -7,7 +7,7 @@ top_end=0
 batch_size=16
 num_epoch=30
 gpu_device=5
-use_mode='finetuned_USE_XORQA_train_en-ru_ko_ja_fi_top0-0_para_finetuned_USE_XORQA_train_en-ru_ko_ja_fi_top1-3_para_0.27margin_2WarmStep_cosLoss_RegFalse_FCFalse_small_0.27_1BatchUpdate_2hard_3shard_cos_HardUpdateFalse_SemiHardUpdateFalse_RegFalse'
+use_mode='teacher_model'
 
 languages='ru_ko_ja'
 # languages='fi'
@@ -17,15 +17,6 @@ learning_rate="1e-3"
 # mse_factor_d_all=("1e0" "1e-1" "1e-2" "1e-3" "1e-4")
 # mse_factor_qd_all=("1e0" "1e-1" "1e-2" "1e-3" "1e-4")
 
-# sup setting
-mse_factor_q_all=("1e-1")
-mse_factor_d_all=("1e0")
-mse_factor_qd_all=("1e-1")
-
-# # unsup setting
-# mse_factor_q_all=("1e0")
-# mse_factor_d_all=("1e0")
-# mse_factor_qd_all=("1e-1")
 
 mse_factor=1000
 shuffle="True"
@@ -53,8 +44,7 @@ top_end=0
 batch_size=16
 num_epoch=30
 gpu_device=7
-use_mode='finetuned_USE_XQUAD_train_en-ar_de_es_ro_ru_th_zh_tr_top0-0_para_finetuned_USE_XQUAD_train_en-ar_de_es_ro_ru_th_zh_tr_top1-3_para_0.24margin_2WarmStep_cosLoss_RegFalse_FCFalse_0.24_1BatchUpdate_1hard_1shard_cos_HardUpdateFalse_SemiHardUpdateFalse_RegFalse'
-
+use_mode='teacher_model'
 
 languages='ar_de_es_ru_th_zh_tr'
 # languages='ro_el_hi_vi'
@@ -63,17 +53,6 @@ learning_rate="1e-3"
 # mse_factor_q_all=("1e0" "1e-1" "1e-2" "1e-3" "1e-4")
 # mse_factor_d_all=("1e0" "1e-1" "1e-2" "1e-3" "1e-4")
 # mse_factor_qd_all=("1e0" "1e-1" "1e-2" "1e-3" "1e-4")
-
-
-# sup setting
-mse_factor_q_all=("1e-3")
-mse_factor_d_all=("1e-1")
-mse_factor_qd_all=("1e-2")
-
-# unsup setting
-# mse_factor_q_all=("1e-4")
-# mse_factor_d_all=("1e-1")
-# mse_factor_qd_all=("1e-2")
 
 mse_factor=1000
 shuffle="True"
@@ -102,8 +81,7 @@ top_end=0
 batch_size=16
 num_epoch=20
 gpu_device=5
-use_mode='finetuned_USE_MLQA_train_en-ar_de_es_zh_para_aligned_finetuned_USE_MLQA_train_en-ar_de_es_zh_top1-3_para_aligned_0.13margin_3WarmStep_cosLoss_1e-05LR_RegFalse_FCFalse_0.13_1BatchUpdate_1hard_1shard_cos_HardUpdateFalse_SemiHardUpdateFalse'
-
+use_mode='teacher_model'
 
 languages='ar_de_es_zh'
 # languages='vi_hi'
@@ -112,16 +90,6 @@ learning_rate="5e-4"
 # mse_factor_q_all=("1e0" "1e-1" "1e-2" "1e-3" "1e-4")
 # mse_factor_d_all=("1e0" "1e-1" "1e-2" "1e-3" "1e-4")
 # mse_factor_qd_all=("1e0" "1e-1" "1e-2" "1e-3" "1e-4")
-
-# sup setting
-mse_factor_q_all=("1e-2")
-mse_factor_d_all=("1e0")
-mse_factor_qd_all=("1e-4")
-
-# unsup setting
-# mse_factor_q_all=("1e-2")
-# mse_factor_d_all=("1e-2")
-# mse_factor_qd_all=("1e-4")
 
 mse_factor=1000
 shuffle="True"
